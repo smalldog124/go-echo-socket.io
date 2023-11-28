e20f7cf8b8450b41ce2dc54b2dc68b7fbc4cf186
smalldog124 <panumars124@gmail.com>
2023-11-28 13:44:10 +0700

    first commit

---
+++ go/main.go
@@ -0,0 +1,63 @@
+package main
+
+import (
+	"log"
+
+	socketio "github.com/googollee/go-socket.io"
+	"github.com/labstack/echo/v4"
+)
+
+func main() {
+	e := echo.New()
+
+	// Middleware for handling CORS
+	e.Use(func(next echo.HandlerFunc) echo.HandlerFunc {
+		return func(c echo.Context) error {
+			c.Response().Header().Set("Access-Control-Allow-Origin", "*")
+			c.Response().Header().Set("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE,OPTIONS")
+			c.Response().Header().Set("Access-Control-Allow-Headers", "Content-Type, Authorization")
+			return next(c)
+		}
+	})
+
+	server := socketio.NewServer(nil)
+
+	//Register the WebSocket endpoint
+	e.GET("/socket.io/", func(c echo.Context) error {
+		server.ServeHTTP(c.Response(), c.Request())
+		return nil
+	})
+
+	// Handle WebSocket connections
+	server.OnConnect("/", func(s socketio.Conn) error {
+		log.Println("Client socket connected:", s.ID())
+		return nil
+	})
+
+	// Handle "order" events from clients
+	server.OnEvent("/", "order", func(s socketio.Conn, msg string) {
+		log.Println("Order received:", msg)
+
+		// Emitting a "shop" event to all connected clients
+		server.BroadcastToRoom("/", "shop", msg)
+	})
+
+	// Handle WebSocket disconnections
+	server.OnDisconnect("/", func(s socketio.Conn, reason string) {
+		log.Println("Client socket disconnected:", s.ID(), reason)
+	})
+
+	go server.Serve()
+	defer server.Close()
+
+	e.HideBanner = true
+
+	e.Static("/", "../web")
+	e.Any("/socket.io/", func(c echo.Context) error {
+		server.ServeHTTP(c.Response(), c.Request())
+		return nil
+	})
+
+	// Start the Echo server
+	e.Logger.Fatal(e.Start(":3000"))
+}
---
+++ readme.md
@@ -0,0 +1,22 @@
+# Goloag echo-socket.io
+ที่ไม่ได้เพราะ `github.com/googollee/go-socket.io` ต้องใช้ sockent.io v2 เท่านั้น
+
+## Run
+```
+$ cd go
+$ go mod tidy
+$ go run main.go
+```
+
+go to http://localhost:3000/index.html
+
+## Dependency
+### go
+github.com/googollee/go-socket.io
+
+github.com/labstack/echo/v4
+
+### javascript
+```js
+<script src="https://cdnjs.cloudflare.com/ajax/libs/socket.io/2.1.1/socket.io.js"></script>
+```
